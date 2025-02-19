63f5ef38ed7ce7b49473337980e999347e6ddd04
rongkuanjiang <r75jiang@uwaterloo.ca>
2025-02-19 15:24:13 -0500

    change syntax from callbacks to async await

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const { readFile } = require('fs');
+const { readFile } = require('fs').promises;
 
 
 const app = express();
 
-app.get('/', (request, response) => {
-    readFile('./return.html', 'utf8', (e, html) => {
+app.get('/', async (request, response) => {
+    response.send(await readFile('./return.html', 'utf8'));
+    
+    /*readFile('./return.html', 'utf8', (e, html) => {
         if (e) {
             response.status(500).send();
         }
         response.send(html);
-    });
+    });*/
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('app available on localhost 3000'));
